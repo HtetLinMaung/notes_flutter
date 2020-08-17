984cf52a4b0a5f3864cf2842c0750e85ef76d9a5
Htet Lin Maung <jsthtet96@gmail.com>
2020-08-18 06:05:48 +0630

    create textfield

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
 const kTitleTextStyle = TextStyle(
-  fontSize: 35,
+  fontSize: 35.0,
   fontWeight: FontWeight.w700,
+);
+
+const kNoteCountTextStyle = TextStyle(
+  color: Color(0xffC9C7D5),
+  fontSize: 16.0,
+);
+
+const kSearchDecoration = InputDecoration(
+  filled: true,
+  isDense: true,
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(35),
+    ),
+    borderSide: BorderSide(
+      style: BorderStyle.none,
+      width: 0,
+    ),
+  ),
+  hintText: 'Search your notes',
+  suffixIcon: Icon(
+    Icons.search,
+    color: Colors.grey,
+  ),
 );
 
 class HomeScreen extends StatelessWidget {
@@ -10,22 +34,44 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 24.0,
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 24.0),
-                child: Text(
-                  'Notes',
-                  style: kTitleTextStyle,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: Text(
+                    'Notes',
+                    style: kTitleTextStyle,
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  '0 notes',
+                  style: kNoteCountTextStyle,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15.0,
+                  ),
+                  child: TextField(
+                    decoration: kSearchDecoration,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
